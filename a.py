c5cc544a7219d4faca430f940c971c000596b3fb
veaari <ved.adapa@gmail.com>
2025-01-09 19:43:51 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+greet = lambda name : print(f"good morning {name}!")
+
+
+# In[4]:
+
+
+product = lambda a,b,c : a*b*c
+
+
+# In[ ]:
+
+
+
+
+
+# In[10]:
+
+
+even = lambda L : [x for x in L if x%2 == 0]
+
+
+# In[11]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
